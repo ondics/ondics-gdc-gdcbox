8bb5648b3e8319641b5592ffb47f8cd0666bd2c8
Wolfgang Git Clauss <githubler@ondics.de>
2012-08-15 11:18:45 +0200

    platform-abhaengigkeit aufgenommen, raspberry-pi i2c

--- appstore/appstore.php
+++ appstore/appstore.php
@@ -6,18 +6,48 @@
     // später können anstelle .inc auch .zip dazukommen für mehrere dateien (incl.
     // bilder etc.)
     $applist = array (
-        array( 'name'=>'Lufft WS600',       'ver'=>'0.0.1', 'file'=>'lufft_ws600.inc' ),
-        array( 'name'=>'Lufft L2P',         'ver'=>'0.1.0', 'file'=>'lufft_l2p.inc' ),        
-        array( 'name'=>'AVR Net IO',        'ver'=>'1.0.0', 'file'=>'avrnetio.inc' ),        
-        array( 'name'=>'Host Ping',         'ver'=>'0.0.1', 'file'=>'hostping.inc' ),        
-        array( 'name'=>'Testdevice',        'ver'=>'0.0.1', 'file'=>'testdevice.inc' ),
-        array( 'name'=>'Testdevice Mini',   'ver'=>'1.0.0', 'file'=>'testdevice_mini.inc' )        
+        array(  'name'=>'Lufft WS600',
+                'ver'=>'0.0.1',
+                'platforms'=>'ANY',
+                'file'=>'lufft_ws600.inc' ),
+        array(  'name'=>'Lufft L2P',
+                'ver'=>'0.1.0',
+                'platforms'=>'ANY',
+                'file'=>'lufft_l2p.inc' ),        
+        array(  'name'=>'AVR Net IO',
+                'ver'=>'1.0.0',
+                'platforms'=>'ANY',
+                'file'=>'avrnetio.inc' ),        
+        array(  'name'=>'Host Ping',
+                'ver'=>'0.0.1', 
+                'platforms'=>'ANY',
+                'file'=>'hostping.inc' ),
+        array(  'name'=>'Raspberry PI I2C',
+                'ver'=>'0.0.1', 
+                'platforms'=>'raspberry-pi',
+                'file'=>'rasppi-i2c.inc' ),        
+        array(  'name'=>'Testdevice',
+                'ver'=>'0.0.1',
+                'platforms'=>'ANY',
+                'file'=>'testdevice.inc' ),
+        array(  'name'=>'Testdevice Mini',
+                'ver'=>'1.0.0',
+                'platforms'=>'ANY',
+                'file'=>'testdevice_mini.inc' ),
     );
 
-
-    switch ($_GET['action']) {
-        case 'applist': 
-            echo json_encode($applist);
+    $action=isset($_GET['action'])?htmlentities($_GET['action'],ENT_QUOTES):'';
+    $machine_os=isset($_GET['machine_os'])?htmlentities($_GET['machine_os'],ENT_QUOTES):'';
+    
+    switch ($action) {
+        case 'applist':
+            $applist_platform=array();
+            foreach($applist as $app) {
+                $platforms=explode(",",$app["platforms"]);
+                if ($platforms[0]=="ANY" || in_array($machine_os,$platforms))
+                    $applist_platform[]=$app;
+            }
+            echo json_encode($applist_platform);
             break;
         case 'download':
             $appfile=$_GET["appfile"];
