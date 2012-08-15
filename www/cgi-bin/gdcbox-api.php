311d68062c64678606b0f3fc43ce47c97b0dda74
Wolfgang Git Clauss <githubler@ondics.de>
2012-08-15 10:47:34 +0200

    platform-includes zentralisiert

--- www/cgi-bin/gdcbox-api.php
+++ www/cgi-bin/gdcbox-api.php
@@ -4,37 +4,13 @@
     //  GDCBox API
     //  (C) Ondics,2012
     */
-
-    // basepath and baseurl depend on machine (default is gdcbox);
-    $basepath="/www-wifunbox";  
-    $baseurl="";
-    $machine="srv1";
-    if ($machine=="srv1") {
-        // ...for use on srv1.ondics.de
-        $basepath="/home/clauss/git-repos/ondics-gdc-gdcbox/www";
-        $baseurl="/gdcbox";
-    }
-    // now the real paths
-    $database=$basepath.'/gdcbox/gdcbox-db.sqlite';
-    $classinc=$basepath.'/gdcbox/classes.inc';
-    $myurl=$baseurl."/cgi-bin/gdcbox-api.php";
-    
-    // appstore access
-    $appstore_url="http://srv1.ondics.de/gdcbox/appstore/appstore.php";
-    $appstore_user="appstore";
-    $appstore_pass="appstore";
-    $apppath=$basepath."/gdcbox/apps";
-    
-    // logging
-    $api_logfile="/tmp/gdcbox_api.log";
-    
+    require_once("../gdcbox/platforms.inc");
 
     // datenbankzugriff herstellen
-    if (! ($pdo=new PDO('sqlite:'.$database)) ) {
+    if (! ($pdo=new PDO('sqlite:'.$env["database"])) ) {
         echo "<html><body>Fehler: DB-Zugriff</body></html>";
         exit;
     } 
-
 
     //////////////////////////////////////////////
     // declare some functions for global use
@@ -43,11 +19,7 @@
         return isset($_GET[$key])?htmlentities($_GET[$key],ENT_QUOTES):''; 
     }
 
-    require_once($classinc);
-
-    // api-function
-    $cmd=getValueFromURLSave('cmd');
-
+    require_once($env["classinc"]);
     // save errors here
     $error="";
     
@@ -56,12 +28,13 @@
     // main program starts here 
     //
     //////////////////////////////////////////
-     
+    // get api-function
+    $cmd=getValueFromURLSave('cmd');
     switch ($cmd) {
     case 'help':
         echo "<html><head><title>GDCBox-API Help</title></head><body>";
         echo "<h1>GDCBox-API Help</h1>";
-        echo "<p>API-Call: $myurl?cmd=&lt;command&gt;{&key=value}...</p>";
+        echo "<p>API-Call: ".$env["api_url"]."?cmd=&lt;command&gt;{&key=value}...</p>";
         echo "<p>Valid API-Commands are:</p>";
         echo "<ul><li>cmd=getvalues&device_id=&lt;id&gt;</li>";
         echo "<li>cmd=lastlog</li>";
@@ -79,7 +52,6 @@
         $error=Device::loadValuesFromDB($device_id,$values,$timestamp);
         if ($error!="") break;
         echo json_encode(array("timestamp"=>$timestamp,"values"=>$values));
-        
         break;
 
 /*
@@ -109,14 +81,14 @@
 
 
     default:
-        $error='command not valid.';
+        $error='api command not valid.';
     }
 
     if ($error != "") {
         $logline=$_SERVER['REMOTE_ADDR']." ".date("Y-m-d H:i:s")." ".$error."\n";
-        file_put_contents($api_logfile,$logline,FILE_APPEND);
+        file_put_contents($env["api_logfile"],$logline,FILE_APPEND);
         echo "<html><head><title>GDC-Box API: Error</title></head><body>";
-        echo '<p>Error:'.$error.'</p><p>try <a href="'.$myurl.'?cmd=help">help</a></p>';
+        echo '<p>Error:'.$error.'</p><p>try <a href="'.$env["api_url"].'?cmd=help">help</a></p>';
         echo '</body></html>';
     } 
 
